fe1ce90363122cd9098e26dfa645f15d5251b545
Ezequiel-Souza-Arraigada <esouza@uade.edu.ar>
2024-11-17 16:04:42 -0300

    Commit final

---
+++ script.js
@@ -0,0 +1,5 @@
+
+document.getElementById('consultaForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Evita el envío real del formulario
+    alert("¡Gracias por enviar tu consulta! Te responderemos a la brevedad.");
+});
